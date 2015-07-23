6a966beb477ce020e524c5e62b4d7d14d71a506f
PETPETRO <peterpetrovaj@gmail.com>
2015-07-23 09:37:28 +0200

    zmena
    aktualizovane

--- js/hangman.js
+++ js/hangman.js
@@ -1,19 +1,21 @@
-// Vytvorte funkciu getGuessedWord(secret, lettersGuessed), ktorá
-// bude mať dva parametre: secret - slovo, ktoré má byť uhádnuté, a lettersGuessed - reťazec reprezentovaný písmenami, ktoré už boli úhadnuté
-// vráti reťazec, ktorý vznikne z reťazca secret nahradením všetkých neuhádnutých písmen (tie, ktoré sa nenachádzajú v reťazci lettersGuessed) znakom ‘_’
-// Príklad použitia:
-// console.log( getGuessedWord("container", "arpxtxgoieyu") )
-// "_o_tai_er"
+//'use strict';
+var elementSecret= document.getElementById('secret');   // represents element with guessed letters 
+var secret='JAVASCRIPT';                                // word we try to guess
+var guessedLetters='';                                  // guessed letters
+var counter=1;
+var group= document.getElementById('alphabet');         // represents element with alphabet buttons
+var image=document.getElementById('phase');             // represents image
+var restart = document.getElementById('restart');       // represents restart button
 
-// getGuessedWord()
 
+// function returns string of letters, where non-guesed letters are represents as _
 
-function getGuessedWord(secret, lettersGuessed)
+function getGuessedWord(secret, guessedLetters)      
 {
   var result='';
   for(var c of secret)
     {
-      if(lettersGuessed.indexOf(c)>=0)
+      if(guessedLetters.indexOf(c)>=0)
         {
           result+=c;
         }
@@ -23,82 +25,121 @@ function getGuessedWord(secret, lettersGuessed)
   return result;
 }
 
-// Zadeklarujte dve globalne premenne:
-// secret - hadane slovo, inicializujte na hodnotu "javascript"
-// guessedLetters - doteraz uhadnute pismena, inicializujte na prazdny retazec
+
+// set the result of function getGuessedWord into element
+
+//elementSecret.textContent= getGuessedWord(secret, guessedLetters);
 
 
 
-var x= document.getElementById('secret');
-var secret='JAVASCRIPT';
-var lettersGuessed='';
-x.textContent= getGuessedWord(secret, lettersGuessed);
-var counter=1;
 
-// for(i=65; i<=90 ;i++){
-
-// 	var c=String.fromCharCode(i);
-// 	console.log(c);
-// }
+// when click on letter button
 
 
-function onClick(event){
-	var el= event.target;
-	//console.log( el.textContent);
-	//deactivate button
-	el.setAttribute('disabled','disabled');
-	//update letters guessed
-	lettersGuessed+=el.textContent;
-	//update
-	x.textContent= getGuessedWord(secret, lettersGuessed);
-	
-	
-	
-    	//update image
-      if(x.textContent.search(el.textContent)<0)
-        {
-        	if(counter<5){
-        		counter++;
-          document.getElementById('phase').setAttribute("src","images/phase"+counter+".png");
-          console.log(counter);
-        	}
-        	else if (counter == 5){
-        		alert('Game over!');
-        		for(var btn of document.getElementById('alphabet')){
-          	btn.setAttribute('disabled','disabled');
-          }
-
-          // else if(secret==x.textContent){
-          // 	alert('WELL DONE!');
-          // }
-        	}
+function update(){
 
 
-        }
+    var current= elementSecret.textContent;                         // add to guessedLetters   //refresh guessed letters
+   elementSecret.textContent=getGuessedWord(secret, guessedLetters);
+
+  if( current == getGuessedWord(secret, guessedLetters)){    //if wrong letter 
+    counter++; 
+    elementSecret.textContent=getGuessedWord(secret, guessedLetters);                                                          
+    if (counter<6)
+    image.setAttribute('src','images/phase'+ counter +'.png');            //change image
+  }
+
+  if(counter==5){                                                         //if 5 times wrong
+    elementSecret.textContent = getGuessedWord(secret, secret);           //show gueessed word
+    alert('Game over !');                                                 
+    for (var btn of document.getElementById('alphabet').childNodes){
+      btn.setAttribute('disabled','disabled');                            // disabled all letter buttons to click  
+    }
+  }
+
+  if(getGuessedWord(secret, guessedLetters)==secret){
+    alert('Well done !');
+    for (var btn of document.getElementById('alphabet').childNodes){
+      btn.setAttribute('disabled','disabled');                            // disabled all letter buttons to click 
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function clickOnLetter(event){
+  var elementLetter=event.target;                                       //butoon of clicked letter
+  guessedLetters+= elementLetter.textContent;
+  update();
+  elementLetter.setAttribute('disabled','disabled');                      //disabled to click on button again
+}
+
+
+
+ function reset(){
+  var guessedLetters='';
+  for (var btn of document.getElementById('alphabet').childNodes){
+      btn.removeAttribute('disabled');
+  }
+  elementSecret.textContent=getGuessedWord(secret, guessedLetters);
+  counter=1;
+  console.log(elementSecret.textContent);
+  update();
+    }
+
+
+  // guessedLetters = "";
+  // elementSecret.textContent = getGuessedWord(secret, lettersGuessed);
+  // for (var btn of document.getElementById('alphabet').childNodes){
+  //     btn.setAttribute('disabled','active');
+    
+    
+
+
+
+
+
+
+function play(){
+
+  //elementSecret.textContent= getGuessedWord(secret, guessedLetters);
+  // create buttons A-Z
+  for(var c of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'){                                                    // for every letter of this string
+                                          var btn=document.createElement('button');             // create element button
+                                          btn.setAttribute('type','button');                    // set the type of element
+                                          btn.setAttribute('class','btn btn-default');          // set the type of button
+                                          btn.textContent=c;                                    // set content of each button on letter 
+                                          btn.addEventListener('click',clickOnLetter);                // add listener on button on event 'click' 
+                                          group.appendChild(btn);                               // adds the element button to the end of the list of child nodes 
+                                           
+  }
+  
+  var restart = document.getElementById('restart');
+  restart.addEventListener('click',reset);
+
+  update();
      
-
-
-}
-
-//initialization
-// var el=document
-window.onload=function(){
-
-
-var group= document.getElementById('alphabet');
-
-for(var c of 'ABCDEFGHIJKLMNOPQRSTUVXYZ'){
-	var btn=document.createElement('button');
-btn.setAttribute('type',"button");
-btn.setAttribute("class","btn btn-default");
-btn.addEventListener('click',onClick);
-btn.textContent=c;
-
-group.appendChild(btn);
+    
 
 }
-}
 
-console.log("initialize");
+
+
+
+
+window.onload=play();
+
+
+
+
 
 
